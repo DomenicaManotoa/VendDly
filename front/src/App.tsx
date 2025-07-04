5b9129f2767b435cd4ae303373f31fe82a56f32e
Richitarsh02 <raa.avalos@yavirac.edu.ec>
2025-07-04 14:10:17 -0500

    xd

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Login } from './Controllers/Login';
 import { Button } from 'antd';
+import Clientes from 'componentes/Clientes';
 
 function Home() {
   return (
@@ -19,7 +20,7 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        
+        <Route path="/clientes" element={<Clientes />} />
       </Routes>
     </BrowserRouter>
   );
--- front/src/componentes/Clientes.tsx
+++ front/src/componentes/Clientes.tsx
@@ -1,3 +1,13 @@
 import SidebarCustom from "./Sidebar";
+const Clientes = () => {
+    // Aquí puedes agregar la lógica específica para la página de Clientes
+  return (
+    <div>
+      <h1>Clientes</h1>
+      <SidebarCustom />
+    </div>
+  );
+};
 
-<SidebarCustom />
+export default Clientes;
+
